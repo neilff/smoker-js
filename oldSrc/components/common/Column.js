9192baec6921bce4391f36647e93ced20ae7fe9c
Neil Fenton <neilff@gmail.com>
2016-03-27 23:14:41 -0400

    completed refactor

--- oldSrc/components/common/Column.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Column = ({ children, className = '' }) => {
-  return (
-    <div className={ `m1 p1 ${ className }` }>{ children }</div>
-  );
-};
-
-export default Column;
--- oldSrc/styles/base.js
+++
@@ -1,6 +0,0 @@
-const base = {
-  black: '#231F20',
-  white: '#F6F6F7',
-};
-
-export default base;
